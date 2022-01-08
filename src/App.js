9f5d6edcad1226e579ac54bcbaf86f2fb97a2146
ihab souey <57112177+ihabsouey@users.noreply.github.com>
2022-01-08 15:10:05 +0100

    add footer

--- src/App.js
+++ src/App.js
@@ -1,25 +1,28 @@
 import React from 'react';
 import Search from './components/Search'
-import Affiche from './components/Affiche'
+import Affiche from './components/Home'
 import Details from './components/Details';
 import NotFound from './components/NotFound';
 import './App.css';
-import {BrowserRouter,Route} from "react-router-dom"
+import { BrowserRouter, Route } from "react-router-dom"
 function App() {
   return (
     <div className="App">
-     
-     
+
+
       <BrowserRouter>
-      <switch>
-         <Route exact path="/Liste-des-films-avec-React-/" component={Affiche} />
-        <Route exact path="/search" component={Search} />
-         <Route exact path="/Liste-des-films-avec-React-/search" component={Search} />
-        <Route exact path="/details/:id" component={Details} />
-        <Route exact path="/Liste-des-films-avec-React-/details/:id" component={Details} />
-        <Route path="/Liste-des-films-avec-React-/404" component={NotFound} />
+        <switch>
+          <Route exact path="/Liste-des-films-avec-React-/" component={Affiche} />
+          <Route exact path="/search" component={Search} />
+          <Route exact path="/Liste-des-films-avec-React-/search" component={Search} />
+          <Route exact path="/details/:id" component={Details} />
+          <Route exact path="/Liste-des-films-avec-React-/details/:id" component={Details} />
+          <Route path="/Liste-des-films-avec-React-/404" component={NotFound} />
         </switch>
-      </BrowserRouter> 
+      </BrowserRouter>
+      <footer>
+        © Souey Ihab 2021 , All Right Reserved
+      </footer>
     </div>
   );
 }
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,10 +1,10 @@
 import React, { useEffect } from 'react'
 import { Link } from 'react-router-dom';
-import Table from "../components/Table"
+import Table from "./Table"
 
 import '../App.css';
 import "../../node_modules/bootstrap/dist/css/bootstrap.min.css";
-
+import icon from '../assets/favicon.ico'
 function Affiche() {
     const API_LIST = 'https://api.themoviedb.org/3/movie/popular?api_key=e3e3d29c3d379923c3ef3321c35b73c0&language=fr&page=1'
     const [films, setFilms] = React.useState([]);
@@ -21,18 +21,23 @@ function Affiche() {
 
     return (
         <div>
-        <header>
+            <header>
+
                 <nav>
-                    <Link to="search" >
-                        <button className="btn btn-primary" id="btnr">
-                            Recherche des films &nbsp;
-                            <img src="https://www.freeiconspng.com/uploads/search-icon-png-21.png" width="35" alt="Search Free Icon Image" />
-                        </button>
-                    </Link>
+                    <div >
+                       
+                        <Link to="search" >
+                            <button className="btn btn-primary" id="btnr">
+                                Recherche des films &nbsp;
+                                <img src="https://www.freeiconspng.com/uploads/search-icon-png-21.png" width="35" alt="Search Free Icon Image" />
+                            </button>
+                        </Link>
+
+                    </div>
                 </nav>
 
                 <div>
-                <h1 id="headerTitle"> Liste des films populaires  </h1>
+                    <h1 id="headerTitle"> Liste des films populaires  </h1>
                 </div>
 
             </header>
@@ -40,12 +45,13 @@ function Affiche() {
 
             <div className='container'>
                 <br /><br />
-             
+
 
                 <br /> <br /> <br />
-                
+
                 <Table films={films} />
             </div>
+           
         </div>
     )
 
